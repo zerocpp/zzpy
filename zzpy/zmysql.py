66cdd7a62711c1ebe6865fcf23b1a35b90a2b4fc
zero <songzhipeng@tenez.cn>
2020-08-12 11:22:09 +0800

    release

--- zzpy/zmysql.py
+++ zzpy/zmysql.py
@@ -93,11 +93,15 @@ def mysql_insert(client, sql):
     return cursor.lastrowid if res else 0
 
 
-def mysql_iter_table(client, table, where_condition="", offset_limit=""):
+def mysql_iter_table(client, table, fields=None, where_condition="", offset_limit=""):
     if where_condition:
         if not where_condition.startswith("where") and not where_condition.startswith("WHERE"):
             where_condition = "where "+where_condition
-    sql = f"select * from {table} {where_condition} {offset_limit}"
+    if fields:
+        fields = ",".join(f"`{i}`" for i in fields)
+    else:
+        fields = "*"
+    sql = f"select {fields} from {table} {where_condition} {offset_limit}"
     from pymysql.cursors import SSDictCursor
     cursor = SSDictCursor(client)
     cursor.execute(sql)
