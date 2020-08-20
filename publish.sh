178872f3bcfeb186c1db602faa162705d62e68f4
zero <songzhipeng@tenez.cn>
2020-08-20 11:30:18 +0800

    release

--- publish.sh
+++ publish.sh
@@ -1,15 +1,11 @@
 rm -rf ./zzpy.egg-info
 rm -rf ./dist
 python setup.py sdist
-twine upload dist/*
+# twine upload dist/*
 twine upload dist/* --repository-url https://nexus.wmqhealth.com/repository/pypi-hosted/
 rm -rf ./zzpy.egg-info
 rm -rf ./dist
 git add *
 git commit -m "release"
 git push origin master
-pip3 install zzpy --upgrade -i https://pypi.org/simple
-pip3 install zzpy --upgrade -i https://pypi.org/simple
-pip3 install zzpy --upgrade -i https://pypi.org/simple
-pip3 install zzpy --upgrade -i https://pypi.org/simple
 pip3 install zzpy --upgrade -i https://pypi.org/simple
