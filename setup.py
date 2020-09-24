ec4528afdebd830f2a1df4118ce3d199999b6f69
zero <songzhipeng@tenez.cn>
2020-09-24 13:40:56 +0800

    release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zzpy",
-    version="1.1.31",
+    version="1.1.32",
     keywords=["pip", "zzpy", "utils"],
     description="Python3 utilities",
     long_description="Python3 utilities",
