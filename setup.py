da37db29450e451ac39fda760a6f52abff11cfb2
Zero <songzhipeng@me.com>
2021-05-18 17:03:46 +0800

    release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zzpy",
-    version="1.1.53",
+    version="1.1.54",
     keywords=["pip", "zzpy", "utils"],
     description="Python3 utilities",
     long_description="Python3 utilities",
